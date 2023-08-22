cbfdcb224bf22797a9a64066d62e5492845252da
Edwin Ricardo Gonzalez Luque <edwinricardo.gonzalezluque@hotmail.com>
2023-08-22 17:04:31 -0500

    Avance

---
+++ Usuarios.Datos/ConexionD.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+
+namespace Usuarios.Datos
+{
+    public class ConexionD
+    {
+        private readonly SqlConnection Conexion = new SqlConnection("Server=PC-EDRIGOLU\\SQLEXPRESS;DataBase= PruebaUsuarios;Integrated Security=true");
+
+        public SqlConnection AbrirConexion()
+        {
+            if (Conexion.State == ConnectionState.Closed)
+            {
+                Conexion.Open();
+            }
+            return Conexion;
+        }
+
+        public SqlConnection CerrarConexion()
+        {
+            if (Conexion.State == ConnectionState.Open)
+            {
+                Conexion.Close();
+            }
+            return Conexion;
+        }
+    }
+}
---
+++ Usuarios.Datos/UsuarioD.cs
@@ -0,0 +1,77 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+
+namespace Usuarios.Datos
+{
+    public class UsuarioD
+    {
+        private ConexionD conexion = new ConexionD();
+
+        SqlDataReader leer;
+        readonly DataTable tabla = new DataTable();
+        readonly SqlCommand comando = new SqlCommand();
+
+        public DataTable Mostrar()
+        {
+
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "MostrarProductos";
+            comando.CommandType = CommandType.StoredProcedure;
+            leer = comando.ExecuteReader();
+            tabla.Load(leer);
+            conexion.CerrarConexion();
+            return tabla;
+
+        }
+
+        public void Insertar(string nombre, string desc, string marca, double precio, int stock)
+        {
+            //PROCEDIMNIENTO
+
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "InsetarProductos";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@nombre", nombre);
+            comando.Parameters.AddWithValue("@descrip", desc);
+            comando.Parameters.AddWithValue("@Marca", marca);
+            comando.Parameters.AddWithValue("@precio", precio);
+            comando.Parameters.AddWithValue("@stock", precio);
+
+            comando.ExecuteNonQuery();
+
+            comando.Parameters.Clear();
+
+        }
+
+        public void Editar(string nombre, string desc, string marca, double precio, int stock, int id)
+        {
+
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "EditarProductos";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@nombre", nombre);
+            comando.Parameters.AddWithValue("@descrip", desc);
+            comando.Parameters.AddWithValue("@Marca", marca);
+            comando.Parameters.AddWithValue("@precio", precio);
+            comando.Parameters.AddWithValue("@stock", stock);
+            comando.Parameters.AddWithValue("@id", id);
+
+            comando.ExecuteNonQuery();
+
+            comando.Parameters.Clear();
+        }
+
+        public void Eliminar(int id)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "EliminarProducto";
+            comando.CommandType = CommandType.StoredProcedure;
+
+            comando.Parameters.AddWithValue("@idpro", id);
+
+            comando.ExecuteNonQuery();
+
+            comando.Parameters.Clear();
+        }
+    }
+}
